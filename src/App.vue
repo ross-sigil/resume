9075de1a6059feac2b08d4e52dca0902ccb7f032
ross-sigil <ross@sigil.systems>
2021-05-10 15:55:14 -0500

    More checks done

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <template>
-  <v-app>
+  <v-app v-bind:style='appBack'>
     <v-content>
       <Resume></Resume>
     </v-content>
+    <v-layout justify-center>
+      <Slider></Slider>
+    </v-layout>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Resume from "./views/Resume.vue";
+import Slider from "./components/Slider.vue";
+
 
 export default Vue.extend({
   name: "App",
   components: {
     Resume,
+    Slider,
   },
 
   data: () => ({
     //
   }),
+  computed: {
+    appBack() {
+      return this.$store.state.appBack;
+    },
 });
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,9 +3,32 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const myPlugin = (stor: any) => {
+  stor.subscribe((mutation: any, state: any) => {
+    localStorage.setItem("Store", JSON.stringify(state));
+  });
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    appBack: "background:#133337",
+    defaultApp: "background:#133337",
+    sliderPercent: 0,
+  },
+  mutations: {
+    appBack: (state, payload: string) => (state.appBack = payload),
+    sliderSet: (state, per: number) => (state.sliderPercent = per),
+    setState: (state, payload: any) => {
+      state.sliderPercent = payload.sliderPercent;
+      state.appBack = payload.appBack;
+    },
+
+    setDefault: (state) => {
+      state.sliderPercent = 0;
+      state.appBack = state.defaultApp;
+    },
+  },
   actions: {},
   modules: {},
+  plugins: [myPlugin],
 });
